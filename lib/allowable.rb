3d1e513e012fd7affff46b2db3fe409ea69f6cbb
Isabel <isagonzacr@gmail.com>
2016-10-20 22:15:08 -0600

    Building gemspec
    -Fixing issue with namespace

--- lib/allowable.rb
+++ lib/allowable.rb
@@ -1,49 +1,48 @@
-module Fences
-   module Allowable
-    def self.included(base)
-      base.extend ClassMethods
+ module Allowable
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def allowable
+      belongs_to :allowable, :polymorphic => true
+
+      self.cattr_accessor :implications
+      self.implications = {}
+
+      include Allowable::InstanceMethods
+      # extend Allowable::SingletonMethods
+    end
+  end
+
+  # module SingletonMethods
+  #   def imply(permission, by_permission)
+  #     self.implications[permission] = [] unless self.implications.has_key? permission
+  #     self.implications[permission] |= [by_permission]
+  #   end
+  #
+  #   def allow(*args, &block)
+  #     if block_given?
+  #       implication = Implication.new(args)
+  #       implication.instance_eval(&block)
+  #     end
+  #   end
+  # end
+
+  module InstanceMethods
+    def allow?(permission)
+      self.send(permission) or self.is_implied?(permission) or self.is_implied?(:all)
     end
 
-    module ClassMethods
-      def allowable
-        belongs_to :allowable, :polymorphic => true
-
-        self.cattr_accessor :implications
-        self.implications = {}
-
-        include Fences::Allowable::InstanceMethods
-        extend Fences::Allowable::SingletonMethods
-      end
-    end
-    
-    module SingletonMethods
-      def imply(permission, by_permission)
-        self.implications[permission] = [] unless self.implications.has_key? permission
-        self.implications[permission] |= [by_permission]
-      end
-
-      def allow(*args, &block)
-        if block_given?
-          implication = Implication.new(args)
-          implication.instance_eval(&block)
+    def is_implied?(permission)
+      if self.implications.has_key? permission
+        self.implications[permission].any? do |implied_by_permission|
+          self.send(implied_by_permission)
         end
-      end
-    end
-
-    module InstanceMethods
-      def allow?(permission)
-        self.send(permission) or self.is_implied?(permission) or self.is_implied?(:all)
-      end
-
-      def is_implied?(permission)
-        if self.implications.has_key? permission
-          self.implications[permission].any? do |implied_by_permission|
-            self.send(implied_by_permission)
-          end
-        else
-          false
-        end
+      else
+        false
       end
     end
   end
 end
+
--- lib/allowances.rb
+++ lib/allowances.rb
@@ -1,5 +1,38 @@
 require "allowances/version"
 
 module Allowances
-  # Your code goes here...
+  module Fencable
+    def self.included(base)
+      base.extend ClassMethods
+    end
+    module ClassMethods
+      def fencable
+        has_many :user_personas, :dependent => :delete_all
+        has_many :personas, :through => :user_personas
+
+        has_one :allowance, :as => :allowable, :dependent => :delete
+
+        include Fencable::InstanceMethods
+      end
+    end
+
+    module InstanceMethods
+      def is_allowed_to?(*args)
+        args.flatten.any? do |arg|
+          (allowance and allowance.allow?(arg)) or
+            personas.any? do |persona|
+              (persona.allowance and persona.allowance.allow?(arg))
+            end
+        end
+      end
+      def is_explicitly_allowed_to?(*args)
+        args.flatten.any? do |arg|
+          (allowance and allowance.send(arg)) or
+            personas.any? do |persona|
+              (persona.allowance and persona.allowance.send(arg))
+            end
+        end
+      end
+    end
+  end
 end
